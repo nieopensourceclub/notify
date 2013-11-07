dc328c4b942164bea0e7cd30d670bf19f50ed3e3
nehakumari <nehasaa.14jan@gmail.com>
2013-11-08 00:23:36 +0530

    second commit

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
   </head>
   <body>
    <div class="nav_bar">
+   <div style="overflow: hidden;">
+     <div style="background-color:orange;float:left;width: 800px;height: 100px;">
       <h2>NotifY</h2>
+      </div>
+      <div style="background-color:orange;float:right; width: 800px;height: 100px;">
       <form action="login.php" class="login_form" method="post">
         <input type="text" name="usn" hint="USN">
         <input type="password" name="pass" hint="Password">
         <input type="submit" value="Login">
       </form>
+      </div>
+      </div>
    </div>
   </body>
 </html>
